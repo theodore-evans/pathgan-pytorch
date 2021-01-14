ad44fdc981976d7ce42ddd81cfa3506c98fa275b
Theodore Evans <theodore.evans@dai-labor.de>
2021-01-14 11:20:18 +0100

    built out conditionalnorm skeleton

--- models/generative/ConditionalNorm.py
+++ models/generative/ConditionalNorm.py
@@ -1,17 +1,31 @@
+from typing import Optional
 from torch import Tensor
 import torch.nn as nn
 
 class ConditionalNorm(nn.Module):
-    def __init__(self):
+    def __init__(self, 
+                 in_channels : int,
+                 latent_dim : int, 
+                 inter_dim : int, 
+                 dense_activation : Optional[nn.Module] = nn.ReLU(), 
+                 gamma_activation : Optional[nn.Module] = nn.ReLU(), 
+                 beta_activation : Optional[nn.Module] = None
+                 ) -> None:
         super().__init__()
-        self.dense_layer_1 = None
-        self.dense_layer_1_activation = nn.ReLU()
         
-        self.dense_layer_gamma = None
-        self.dense_layer_gamma_activation = nn.ReLU()
+        out_channels = in_channels
         
-        self.dense_layer_beta = None
-        self.dense_layer_gamma_activation = None
+        self.add_module('dense_layer', nn.Linear(latent_dim, inter_dim))
+        if dense_activation is not None:
+            self.add_module('dense_layer_activation', dense_activation)
+        
+        self.add_module('dense_layer_gamma', nn.Linear(inter_dim, out_channels))
+        if gamma_activation is not None:
+            self.add_module('dense_layer_gamma_activation', gamma_activation)
+        
+        self.add_module('dense_layer_beta', nn.Linear(inter_dim, out_channels))
+        if beta_activation is not None:
+            self.add_module('dense_layer_beta_activation', beta_activation)
 
     def forward(self, input, latent_input) -> Tensor:
         pass
