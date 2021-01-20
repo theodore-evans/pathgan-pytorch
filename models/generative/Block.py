fixed bugs with normalization dependency injection

models/generative/Block.py
@@ -4,17 +4,18 @@ from torch import Tensor
 import torch.nn as nn
 from torch.nn.modules.container import ModuleDict
 
-from .Normalization.AbstractNormalization import AbstractNormalization
-from .Initialization.AbstractInitializer import AbstractInitializer
+from .normalization.AbstractNormalization import AbstractNormalization
+from .initialization.AbstractInitializer import AbstractInitializer
 
 class Block(nn.Module):
     def __init__(self,
                  in_channels: int,
                  out_channels: int,
                  modules: Optional[ModuleDict] = None,
+                 latent_dim: Optional[int] = None,
                  regularization: Optional[Callable[[nn.Module], nn.Module]] = lambda x: x,
                  noise_input: Optional[Callable[[int], nn.Module]] = None,
-                 normalization: Optional[Callable[[int], AbstractNormalization]] = None,  
+                 normalization: Optional[Callable[..., AbstractNormalization]] = None,
                  activation: Optional[nn.Module] = None,
                  initializer: Optional[Callable[[nn.Module], AbstractInitializer]] = None
                  ) -> None:
@@ -32,7 +33,10 @@ class Block(nn.Module):
             self.noise_input = noise_input(out_channels)
         
         if normalization is not None:
-            self.normalization = normalization(out_channels)
+            if latent_dim is not None:
+                self.normalization = normalization(out_channels, latent_dim)
+            else:
+                self.normalization = normalization(out_channels)
             
         if activation is not None:
             self.activation = activation

models/generative/Initialization/AbstractInitializer.py
@@ -10,7 +10,7 @@ class AbstractInitializer(ABC):
         self.module = module
         
     @abstractmethod
-    def initialize_weights(self, initialization: Optional[Callable[[Tensor]]] = None) -> None:
+    def initialize_weights(self, initialization: Optional[Callable[[Tensor], None]] = None) -> None:
         for m in self.module.modules():
             if isinstance(m, (nn.Conv2d, nn.ConvTranspose2d, nn.Linear)):
                 

residual_block_test.py
@@ -1,8 +1,13 @@
 import torch
+import torch.nn as nn
 from models.generative.ResidualBlock import ResidualBlock
 from models.generative.ConvolutionalBlock import ConvolutionalBlock
+from models.generative.normalization.AdaptiveInstanceNormalization import AdaptiveInstanceNormalization
 
-res_block = ResidualBlock(2, ConvolutionalBlock(10, 10, 3, 1, 1))
+res_block = ResidualBlock(2, ConvolutionalBlock(10, 10, 3, 1, 1, 
+                                                latent_dim = 200,
+                                                normalization=AdaptiveInstanceNormalization, 
+                                                activation=nn.LeakyReLU(0.2)))
 print(res_block)
 
 input = torch.ones(10, 10, 3, 3)