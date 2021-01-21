dd34dd308881b300ca1d29f9fad001caf0b51057
Emirhan Karagül <emirhan.karaguel@dai-labor.de>
2021-01-21 12:51:48 +0100

    Add regularizer classes

---
+++ models/generative/Regularization/AbstractRegularizer.py
@@ -0,0 +1,25 @@
+from abc import ABC, abstractmethod
+from typing import Callable
+
+import torch
+import torch.nn as nn
+from torch.tensor import Tensor
+
+
+class AbstractRegularizer(ABC):
+    @abstractmethod
+    def __init__(self, module: nn.Module, regularizer_scale: float = 1e-4):
+        self.module = module
+        self.regularizer_scale = regularizer_scale
+
+    @abstractmethod
+    def get_regularizer_loss(self, loss_function: Callable[[Tensor], Tensor] = None) -> Tensor:
+        if loss_function is None:
+            return torch.zeros(1)
+
+        with torch.enable_grad():
+            loss = torch.zeros(1)
+            for name, param in self.module.named_parameter():
+                if 'bias' not in name and 'Scale' not in name:
+                    loss += self.regularizer_scale * loss_function(param)
+            return loss
---
+++ models/generative/Regularization/OrthogonalRegularizer.py
@@ -0,0 +1,20 @@
+import torch.nn as nn
+import torch
+from torch.tensor import Tensor
+
+from .AbstractRegularizer import AbstractRegularizer
+
+class OrthogonalRegularizerlizer(AbstractRegularizer):        
+    def __init__(self, module: nn.Module):
+        self.module = module
+    
+    def get_regularizer_loss(self) -> None:
+        loss_func = self.get_orthogonality
+        return super().get_regularizer_loss(loss_func)
+
+    @staticmethod
+    def get_orthogonality(param: Tensor) -> Tensor:
+        param_flat = param.view(param.shape[0], -1)
+        sym = torch.mm(param_flat, torch.t(param_flat))
+        sym -= torch.eye(param_flat.shape[0])
+        return sym.abs().sum()
