210bb84f56825bb2f14a9a9534b7cc3a73391877
Theodore Evans <theodore.evans@dai-labor.de>
2021-01-24 21:55:40 +0100

    minor changes to adain

--- models/generative/Normalization/AdaptiveInstanceNormalization.py
+++ models/generative/Normalization/AdaptiveInstanceNormalization.py
@@ -7,56 +7,64 @@ from torch.nn.modules.instancenorm import InstanceNorm2d
 from .AbstractNormalization import AbstractNormalization
 
 class AdaptiveInstanceNormalization(AbstractNormalization, nn.Module):
-    def __init__(self, 
+    def __init__(self,
                  channels : int,
-                 latent_dim : int, 
-                 intermediate_channels : Optional[int] = None, 
-                 dense_activation : Optional[nn.Module] = nn.ReLU(), 
-                 gamma_activation : Optional[nn.Module] = nn.ReLU(), 
+                 latent_dim : int,
+                 intermediate_layer: bool = False,
+                 intermediate_channels : Optional[int] = None,
+                 dense_activation : Optional[nn.Module] = nn.ReLU(),
+                 gamma_activation : Optional[nn.Module] = nn.ReLU(),
                  beta_activation : Optional[nn.Module] = None
                  ) -> None:
         
         nn.Module.__init__(self)
         
         self.latent_dim = latent_dim
-
         self.instance_norm = InstanceNorm2d(channels)
         
-        if intermediate_channels is not None:
+        if intermediate_layer:
+            if intermediate_channels is None:
+                intermediate_channels = (latent_dim + channels) // 2
             self.dense_layer = nn.Linear(latent_dim, intermediate_channels)
             in_channels = intermediate_channels
-            self.dense_activation = dense_activation if dense_activation is not None else lambda x:x
-        else: 
+            self.dense_activation = dense_activation
+        else:
             self.dense_layer = None
             in_channels = latent_dim
             
         out_channels = channels
         
         self.gamma_layer = nn.Linear(in_channels, out_channels)
-        self.gamma_activation = gamma_activation if gamma_activation is not None else lambda x:x
+        self.gamma_activation = gamma_activation
         
         self.beta_layer = nn.Linear(in_channels, out_channels)
-        self.beta_activation = beta_activation if beta_activation is not None else lambda x:x
+        self.beta_activation = beta_activation
 
-    def forward(self, 
+    def forward(self,
                 input_tensor: Tensor,
                 latent_input: Optional[Tensor]
                 ) -> Tensor:
         
         if latent_input is None:
-            batch_size = input_tensor.shape[0]
-            latent_input = torch.zeros(batch_size, self.latent_dim)
+            latent_input = torch.zeros(input_tensor.shape[0], self.latent_dim)
             
         normalized_input = self.instance_norm(input_tensor)
         
         if self.dense_layer is not None:
-            intermediate_result = self.dense_activation(self.dense_layer(latent_input))
+            intermediate_result = self.dense_layer(latent_input)
+            if self.dense_activation is not None:
+                intermediate_result = self.dense_activation(intermediate_result)
         else: intermediate_result = latent_input
         
-        gamma = self.gamma_activation(self.gamma_layer(intermediate_result))
-        beta = self.beta_activation(self.beta_layer(intermediate_result))
+        gamma = self.gamma_layer(intermediate_result)
+        if self.gamma_activation is not None:
+            gamma = self.gamma_activation(gamma)
+            
+        beta = self.beta_layer(intermediate_result)
+        if self.beta_activation is not None:
+            beta = self.beta_activation(beta)
         
-        style_shift_transform = beta[:, : , None , None]
+        style_shift_transform = beta[:, :, None , None]
         style_scale_transform = gamma[:, :, None, None]
         
         transformed_input = style_scale_transform * normalized_input + style_shift_transform
