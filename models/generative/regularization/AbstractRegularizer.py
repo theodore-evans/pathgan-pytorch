90ff7ff29611a75eb76375eafa0d319c36efdbcb
Theodore Evans <theodore.evans@dai-labor.de>
2021-01-28 12:47:55 +0100

    ource /Users/theoevans/anaconda3/bin/activate conda activate pathgan-torch evert "fixed typing and typo in regularization"
    This reverts commit d7133644e921ec00aba385f09e725551a197a7d3.

--- models/generative/regularization/AbstractRegularizer.py
+++ models/generative/regularization/AbstractRegularizer.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-from typing import Callable, Optional
+from typing import Callable
 
 import torch
 import torch.nn as nn
@@ -13,13 +13,13 @@ class AbstractRegularizer(ABC):
         self.regularizer_scale = regularizer_scale
 
     @abstractmethod
-    def get_regularizer_loss(self, loss_function: Optional[Callable[[Tensor], Tensor]] = None) -> Tensor:
+    def get_regularizer_loss(self, loss_function: Callable[[Tensor], Tensor] = None) -> Tensor:
         if loss_function is None:
             return torch.zeros(1)
 
         with torch.enable_grad():
             loss = torch.zeros(1)
-            for name, param in self.module.named_parameters():
+            for name, param in self.module.named_parameter():
                 if 'bias' not in name and 'Scale' not in name:
                     loss += self.regularizer_scale * loss_function(param)
             return loss
