a973283e5caa30b050034f90ea78379d509bbd77
Emirhan Karagül <emirhan.karaguel@dai-labor.de>
2021-02-06 15:28:49 +0100

    Add Adam Opt Parameter Beta 2

--- run_pathgan.py
+++ run_pathgan.py
@@ -21,7 +21,7 @@ epochs = args.epochs
 batch_size = args.batch_size
 
 # To overcome no locks available error
-os.environ['HDF5_USE_FILE_LOCKING']='FALSE'
+os.environ['HDF5_USE_FILE_LOCKING'] = 'FALSE'
 
 # Dataset information.
 dataset_path = args.data_path
@@ -49,6 +49,6 @@ data = Dataset(args.data_path, image_height, image_width,
                image_channels, batch_size, data_type='train')
 
 # TODO: add parameters and beta
-pathgan = PathologyGAN(data, learning_rate_d=learning_rate_d, learning_rate_g=learning_rate_g, beta=beta_1,
+pathgan = PathologyGAN(data, learning_rate_d=learning_rate_d, learning_rate_g=learning_rate_g, beta_1=beta_1, beta_2=beta_2
                        epochs=15, z_dim=z_dim, checkpoin_path=args.weights, gp_coeff=gp_coeff, output_path=data_out_path)
 pathgan.train(False, 5)
